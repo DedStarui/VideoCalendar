e1ad643fbaa8df078691b8ced9b0dcbaaa2174d7
DedStarui <136914518+DedStarui@users.noreply.github.com>
2025-01-25 16:53:25 +0200

    main.py

--- main.py
+++ main.py
@@ -17,24 +17,31 @@ class Widget(QMainWindow):
         self.media = QMediaPlayer(self)
         #налаштуй вивід зображення в Qwidget
         self.media.setVideoOutput(self.ui.widget)
+        self.configure()
         #Завантаж відео що треба програвати
-        vid=QMediaContent(QUrl.fromLocalFile('Video\\6.avi'))
-        self.media.setMedia(vid)
+        #vid=QMediaContent(QUrl.fromLocalFile('Video\\7.avi'))
+        #self.media.setMedia(vid)
         #запусти відео
-        self.media.play()
+        #self.media.play()
         #self.media.stop()
 
-    #def configure(self):
-        #...
+    def configure(self):
+        self.ui.pushButton.clicked.connect(self.media_play)
+        self.ui.pushButton_3.clicked.connect(self.media_stop)
+        self.ui.calendarWidget.selectionChanged.connect(self.get_date)
 
-    #def get_date(self):
-        #...
+    def get_date(self):
+        Day = str(self.ui.calendarWidget.selectedDate().day())
+        self.media.setMedia(QMediaContent(QUrl.fromLocalFile(f'Video\\{Day}.avi')))
+        if self.ui.checkBox.isChecked():
+            self.media_play()
 
-    #def media_play(self):
-        #...
+    def media_play(self):
+        self.media.play()
 
-    #def media_stop(self):
-        #...
+
+    def media_stop(self):
+        self.media.stop()       
 
 
 if __name__ == '__main__':
